56fe63335e204a5a4bab773cf8fe4fb243742123
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-04-21 17:20:04 -0500

    Update servidor2_pyro_gemini.py

--- servidor2_pyro_gemini.py
+++ servidor2_pyro_gemini.py
@@ -19,10 +19,9 @@ class GeminiClient2:
     def lista_productos(self, respuesta, barrio):
         respuesta2 = modelo.generate_content(f"Necesito que con la lista de ingredientes de la siguiete receta, busques los mejores lugares para comprar cada producto en {barrio} Ecuador, debes especificar especificamente el nombre de los lugares: {respuesta}")
     
-        respuesta_l=servidor3(respuesta+"\n"+respuesta2.text)
-        #print(respuesta2.text)
-        print(type(respuesta_l))    
+        respuesta_l=servidor3(respuesta+"\n"+respuesta2.text) 
         return respuesta_l
+        
 daemon = Pyro4.Daemon(host="0.0.0.0", port=9091)
 uri= daemon.register(GeminiClient2,objectId="GeminiClient2")
 daemon.requestLoop()
