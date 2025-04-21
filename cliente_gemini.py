125e286707d4461d253776943e3f39baa17c827c
Jhon Cordova <jcordovam8@est.ups.edu.ec>
2025-04-21 13:35:06 -0500

    Update cliente_gemini.py

--- cliente_gemini.py
+++ cliente_gemini.py
@@ -2,10 +2,19 @@ import Pyro4
 import base64
 
 client = Pyro4.Proxy("PYRO:GeminiClient@192.168.100.66:9090")
+
 input_text = input("Escribe el platillo: ")
 input_barrio = input("Escribe el sector donde vives: ")
+
 respuesta = client.consulta(input_text, input_barrio)
-# Crear un archivo.pdf con la respuesta Crear un archivo.pdf con la respuesta
-respuesta_decoded = base64.b64decode(respuesta['data'])
-with open("respuesta.pdf", "wb") as f:
-    f.write(respuesta_decoded)
+
+if respuesta and 'data' in respuesta:
+    try:
+        respuesta_decoded = base64.b64decode(respuesta['data'])
+        with open("respuesta.pdf", "wb") as f:
+            f.write(respuesta_decoded)
+        print("El archivo 'respuesta.pdf' ha sido recibido y guardado exitosamente.")
+    except Exception as e:
+        print(f"Error al guardar el archivo: {e}")
+else:
+    print("No se recibió el archivo.")
